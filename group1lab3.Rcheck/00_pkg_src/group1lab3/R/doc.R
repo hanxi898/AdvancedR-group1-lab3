e5c19b11c76fd2c5362218f51ef09efa7f355e81
canwa869 <canwa869@student.liu.se>
2024-09-27 20:40:45 +0200

    13

--- group1lab3.Rcheck/00_pkg_src/group1lab3/R/doc.R
+++
@@ -1,6 +0,0 @@
-#' @title group1lab3
-#' @description This package covers two functions -- "euclidean" and "dijkstra".
-#' @details "Euclidean" function computes the greatest common divisor of two numbers. And "dijkstra" function computes the shortest paths from a given starting node to all other nodes in a graph.
-#' @name doc-package
-NULL
-
